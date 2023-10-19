c3a71ee4c5307ff8a09188fbf95ca824a33e94f7
hanDH <76561461+gkseogus@users.noreply.github.com>
2023-10-19 14:38:42 +0900

    [level unrated] Title: OX퀴즈, Time: 0.05 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120907. OX퀴즈/OX퀴즈.py
+++ 프로그래머스/unrated/120907. OX퀴즈/OX퀴즈.py
@@ -10,5 +10,3 @@ def solution(quiz):
         else:
             oxL.append("X")
     return oxL
-        #print(eval((i[0].split("="))[0]), i[1])
-        #print(eval((quiz[0].split("="))[0]), (quiz[0].split("="))[1])
