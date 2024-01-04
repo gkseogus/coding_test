ccc28cea3d87152f1d86df902299ee808e11582e
hanDH <76561461+gkseogus@users.noreply.github.com>
2024-01-04 17:32:09 +0900

    [level unrated] Title: 문자 개수 세기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.py
@@ -0,0 +1,11 @@
+def solution(my_string):
+    result = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    abcL = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    
+    for i in my_string:
+        for j in range(0, len(abcL)):
+            if(i == abcL[j]):
+                print(i.lower(), abcL[j], result[j])
+                result[j] += 1
+    
+    return result
